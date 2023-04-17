739ce822b84e2fde74088400f664efcd9b8bd027
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-17 10:14:38 -0700

    first commit

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace MethodOverloading
+{
+	public class Methods
+	{
+		
+		public static int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public static double Add(double a, double b)
+		{
+			return a + b;
+		}
+		public static decimal Add(decimal a, decimal b)
+		{
+			return a + b;
+		}
+		public static string Add( int a, int b, bool withDollars)
+		{
+			if (withDollars && ( a+ b) != 1)
+			{
+				return $"{a + b} dollars";
+			}
+			else if (withDollars && (a + b) == 1)
+			{
+				return $"{a + b} dollar";
+			}
+			else
+			{
+				return $"{a + b}";
+			}
+				
+
+		}
+
+	}
+		
+}
+
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Methods.Add(5, 7);
+            Console.WriteLine(answer);
+            Console.WriteLine(Methods.Add(2.0, 3.1));
+            Console.WriteLine(Methods.Add(2.0m, 3.1m));
+            Console.WriteLine(Methods.Add(0,0,true));
         }
+        
+
+
+
     }
 }
